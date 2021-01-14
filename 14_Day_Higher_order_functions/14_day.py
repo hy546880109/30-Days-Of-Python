8deaae38d5b18c62b5a2e78f405f731c7b81a724
huny <hy546880109@qq.com>
2021-01-14 13:27:42 +0800

    add 14day

--- 14_Day_Higher_order_functions/14_day.py
+++ 14_Day_Higher_order_functions/14_day.py
@@ -23,35 +23,52 @@
 # print(big_message(get_message)())
 
 # 装饰器@的进阶
-import time
-def big_message(func):
-    def warpper():
-        f = func()
-        t = f.title()
-        return t
-    return warpper
-def len_message(func):
-    def warpper():
-        f = func()
-        t = len(f)
-        return t
-    return warpper
+# import time
+# def big_message(func):
+#     def warpper():
+#         f = func()
+#         t = f.title()
+#         print(t)
+#         return t
+#     return warpper
 
-def time_message(func):
-    def warpper():
-        t = time.time()
-        f = func()
-        t1 = time.time()
-        run_time = t1 - t
-        print(run_time) 
-    return warpper
+# def len_message(func):
+#     def warpper():
+#         f = func()
+#         t = len(f)
+#         print(t)
+#         return t
+#     return warpper
+
+# def time_message(func):
+#     def warpper():
+#         t = time.time()
+#         f = func()
+#         t1 = time.time()
+#         run_time = t1 - t
+#         # print(run_time) 
+#         return run_time
+#     return warpper
 
 # @time_message
-@len_message
-@big_message
-def get_message():
-    return 'hello,world!'
+# @len_message
+# @big_message
+# def get_message():
+#     print('hello,world!')
+#     return 'hello,world!'
 
-print(get_message())
+# print(get_message())
+
+# Map函数
+
+# num = [x for x in range(6)]
+# def square(x):
+#     return x**2
+# num_square = map(square, num)
+# print(list(num_square))
+
+# nums = map(lambda x:x**2,num)
+# print(list(nums))
+        
 
 #
