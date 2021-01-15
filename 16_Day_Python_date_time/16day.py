ebf258cc2c3ddfd8cfa15e48ae4321ea0acd69d2
huny <hy546880109@qq.com>
2021-01-15 08:56:07 +0800

    add 16day

---
+++ 16_Day_Python_date_time/16day.py
@@ -0,0 +1,29 @@
+# 练习
+
+# 1.从 datetime 模块获取当前日期、月份、年份、小时、分钟和时间戳
+
+from datetime import datetime
+now = datetime.now()
+month = now.month
+year = now.year
+hour = now.hour
+minute = now.minute
+second = now.second
+time = now.time
+print(now)
+print(month)
+print(year)
+print(hour)
+print(minute)
+print(second)
+print(time)
+
+
+# 2.使用此格式设置当前日期的格式: “% m/% d/% y,% h:% m:% s”)
+
+# 3.今天是2019年12月5日。将这个时间字符串更改为 time
+
+# 4.计算现在和新年之间的时差
+
+# 5.计算1970年1月1日与现在的时差
+
